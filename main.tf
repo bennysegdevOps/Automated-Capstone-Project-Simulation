539684edad395660fb3632df0b2fa9522bd4531f
olusegun-odede <olusegun.odede@cloudhight.com>
2023-05-02 08:50:21 -0500

    updated variables

--- main.tf
+++ main.tf
@@ -759,14 +759,14 @@ resource "aws_cloudwatch_metric_alarm" "ec2_cpu_alarm" {
 # Creating Target Group
 resource "aws_lb_target_group" "target_group" {
   name_prefix      = "alb-tg"
-  port             = 80
+  port             = var.http_port
   protocol         = "HTTP"
   vpc_id           = aws_vpc.vpc.id
 
   health_check {
     interval            = 60
     path                = "/indextest.html"
-    port                = 80
+    port                = var.http_port
     protocol            = "HTTP"
     timeout             = 30
     healthy_threshold   = 3
@@ -778,7 +778,7 @@ resource "aws_lb_target_group" "target_group" {
 resource "aws_lb_target_group_attachment" "target_group_att" {
   target_group_arn = aws_lb_target_group.target_group.arn
   target_id        = aws_instance.wordpress_webserver.id
-  port             = 80
+  port             = var.http_port
 }
 
 # SNS Alarm topic 
@@ -977,7 +977,7 @@ resource "aws_lb_listener" "eu2acp_lb_listener" {
 # Creating a Load balancer Listener for https access
 resource "aws_lb_listener" "eu2acp_lb_listener1" {
   load_balancer_arn = aws_lb.eu2acp_alb.arn
-  port              = "443"
+  port              = var.https_port
   protocol          = "HTTPS"
   ssl_policy        = "ELBSecurityPolicy-2016-08"
   certificate_arn   = "${aws_acm_certificate.acm_certificate.arn}"
